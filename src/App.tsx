3fd34932e22dd193cb2355f55572cf37b89ab7ae
lakshay <lakshayg977@gmail.com>
2026-03-07 13:37:48 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
+          //lakshay testing
           count is {count}
         </button>
         <p>
